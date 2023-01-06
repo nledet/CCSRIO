db8ffa31ab1b65ab482cfd5e2954ffef0b1b39e0
nledet <noemie.ledet@gmail.com>
2023-01-06 14:04:41 +0100

    tests

--- Cesar/dictionnaire.sh
+++ Cesar/dictionnaire.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-awk -F, 'NR > 1 {print $1,$2, $3, $4,$5,$6}' srioeye_stolen_data.csv | while read -r fname lname birth embauche children parent ; do
+awk -F, 'NR > 1 {print $1,$2, $3, $4,$5,$6}' tests.csv | while read -r fname lname birth embauche children parent ; do
   # Process the data for each employee
 
 # Déclaration de l'array contenant les données
 data=($fname $lname $birth $embauche $children $parent)
-
+debut=$(date +%s)
 # Génération de toutes les permutations et écriture dans le fichier
 for i in "${data[@]}"
 do
@@ -25,4 +25,8 @@ do
     done
   done
 done
+
 done
+fin=$(date +%s)
+duree=$(( $fin - $debut ))
+echo "latence moyenne : $duree secondes"
