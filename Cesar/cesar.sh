8e53e34aac976855b2c3264d192c38458a8f4c69
nledet <noemie.ledet@gmail.com>
2023-01-06 11:22:16 +0100

    tentative

--- Cesar/cesar.sh
+++ Cesar/cesar.sh
@@ -1,20 +1,9 @@
 #!/bin/bash
 
-
-cryptName(){
-    echo cryptName $1
-    local modif=$(echo $1 | tr '[A-Za-z0-9]' '[D-ZA-Cd-za-c3-90-2]')
-    mv "$2/$1" "$2/$modif"
-
-}
-
 cryptFile(){
-    echo cryptFile $1
+    echo cryptFile dictionary.txt
     #cipher de cesar de 3
-    echo $(cat "$2/$1" | tr '[A-Za-z0-9]' '[E-ZA-De-za-d4-90-3]') > "$2/$1"
+    echo $(cat "dictionary.txt" | tr '[A-Za-z0-9]' '[E-ZA-De-za-d4-90-3]') > "dictCrypte.txt"
 }
 
-cryptFile dictionnaire.sh $1
-
-
-
+cryptFile
--- Cesar/dictionnaire.sh
+++ Cesar/dictionnaire.sh
@@ -2,40 +2,20 @@
 awk -F, 'NR > 1 {print $1,$2, $3, $4,$5,$6}' srioeye_stolen_data.csv | while read -r fname lname birth embauche children parent ; do
   # Process the data for each employee
 
-  for x in $fname; do
-    for y in $lname; do
-      for z in $birth; do
-        for a in $embauche; do
-          for b in $children; do
-            for c in $parent; do
-              echo "$x$y$z$a$b$c"
-              echo "$y$x$z$a$b$c"
-              echo "$z$y$x$a$b$c"
-              echo "$x$z$y$a$b$c"
-              echo "$y$z$x$a$b$c"
-              echo "$z$x$y$a$b$c"
-              echo "$x$y$z$c$b$a"
-              echo "$y$x$z$c$b$a"
-              echo "$z$y$x$c$b$a"
-              echo "$x$z$y$c$b$a"
-              echo "$y$z$x$c$b$a"
-              echo "$z$x$y$c$b$a"
-              echo "$x$y$z$b$c$a"
-              echo "$a$y$x$b$c$z"
-              echo "$a$z$y$b$c$x"
-              echo "$a$x$z$b$c$y"
-              echo "$a$y$z$b$c$x"
-              echo "$a$z$x$b$c$y"
-              echo "$a$x$y$b$c$z"
-              echo "$a$y$z$c$b$x"
-              echo "$b$y$x$c$a$z"
-              echo "$b$z$y$c$a$x"
-              echo "$b$x$z$c$a$y"
-            done
-          done
-        done
-      done
-    done
-  done
+data=($fname $lname $birth $embauche $children $parent)
+size=${#data[@]}
+count=0
+exec 3>dictionary.txt
+while [ $count -lt 720 ]
+do
+  random=$(echo $(seq $size) | shuf) 
+  count=$((count+1))
 
-done > dictionary.txt
+  for i in $random
+  do 
+    echo -n "${data[$i-1]}" 1>&3
+  done 
+  echo 1>&3
+done 
+exec 3>&-
+done
