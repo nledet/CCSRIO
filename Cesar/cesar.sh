8b608fcaa56e7a797cf2c610dfb89616f87f6208
nledet <noemie.ledet@gmail.com>
2023-01-06 11:29:46 +0100

    crypt fonctionnel

--- Cesar/cesar.sh
+++ Cesar/cesar.sh
@@ -3,7 +3,7 @@
 cryptFile(){
     echo cryptFile dictionary.txt
     #cipher de cesar de 3
-    echo $(cat "dictionary.txt" | tr '[A-Za-z0-9]' '[E-ZA-De-za-d4-90-3]') > "dictCrypte.txt"
+    echo $(cat "dictionary.txt" | tr '[AB-DEF-HIJ-NOP-TUV-XYZab-def-hij-nop-tuvwxyz0-9]' '[DF-HHJ-LLN-RRT-XXZABBDdf-hhj-lln-rrt-xxzabbd4-90-3]') > "dictCrypte.txt"
 }
 
 cryptFile
